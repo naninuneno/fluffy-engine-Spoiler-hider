f0c7245fcc57b72c34fa0db2bcb5d28a787c15b9
nbrowning <n.lalilulelo@gmail.com>
2017-06-13 23:37:11 +0100

    Added better spoiler detection, bit of optimisation (I think, but still bad), and message customisation to know which area the spoiler belongs to e.g. which tv show

--- content.js
+++ content.js
@@ -3,17 +3,26 @@ chrome.runtime.sendMessage({method: "storage"}, function(response) {
   blurSpoilers(response.storage);
 });
 
+// TODO: has a 4-nested for loop, disgusting
 function blurSpoilers(storage) {
-  var spoilerText = "Potential spoilers! Mouse over to view original";
+  var spoilerTextTemplate = "Potential spoilers for {0}! Mouse over to view original";
+  var spoilerSetClass = "spoiler-set"
   
   // TODO: make work for other elements
-  var texts = document.getElementsByTagName('p');
-  var potentialSpoilers = [];
+  // find elements that haven't already had spoiler set
+  var texts = document.querySelectorAll('p:not(.' + spoilerSetClass + ')');
+  
+  // for each show
   for (i = 0; i < storage.length; i++) {
+    var potentialSpoilers = [];
     var storedShow = storage[i];
     if (!storedShow.enabled) {
       continue;
     }
+    
+    // custom spoiler text for the show for some clarity
+    var spoilerText = spoilerTextTemplate.format(storedShow.name);
+    
     storedShow.cast.forEach(function(character) {
       character.names.forEach(function(name) {
         // remove quotations from character nicknames
@@ -22,53 +31,75 @@ function blurSpoilers(storage) {
           name.text = name.text.substring(1, name.text.length - 1);
         }
         if (name.enabled) {
-          potentialSpoilers.push(name.text);
+          // for case-insensitive comparison
+          potentialSpoilers.push(name.text.toUpperCase());
         }
       });
     });
-  }
+  
 
-  // TODO: case-insensitive comparison
-  // TODO: poor performance - alternative?
-  // TODO: slow to start - waits for page to load
-  // TODO: doesn't handle new content after page already loaded e.g. new reddit page in RES
-  // TODO: partial match - contains check probably e.g. "Jon" -> "Jon/..", "Jon's"
-  for (i = 0; i < texts.length; i++) {
-    // innerHTML rather than textContent so that links etc. are preserved
-    var originalHtml = texts[i].innerHTML;
-    var isSpoiler = false;
-    
-    for (j = 0; j < potentialSpoilers.length; j++) {
-      var spoiler = potentialSpoilers[j];
-    
-      var words = texts[i].textContent.split(" ");
-      
-      for (k = 0; k < words.length; k++) {
-        var word = words[k];
+    // TODO: poor performance - alternative?
+    // TODO: slow to start - waits for page to load
+    // TODO: doesn't handle new content after page already loaded e.g. new reddit page in RES
+    // for each text on page
+    for (j = 0; j < texts.length; j++) {
+      // innerHTML rather than textContent so that links etc. are preserved
+      var originalHtml = texts[j].innerHTML;
+      var isSpoiler = false;
+      // for case-insensitive comparison
+      var elementText = texts[j].textContent.toUpperCase();
+      // remove common concatenations to the target (possessive, /(another name))
+      elementText = elementText.replace(/'s|'S|\/[^ ]*/g, '');
+
+      // for each potential spoiler text word (e.g. each word of character name)
+      for (k = 0; k < potentialSpoilers.length; k++) {
+        var spoiler = potentialSpoilers[k];
+
+        var words = elementText.split(" ");
         
-        if (word == spoiler) {
-          texts[i].textContent = spoilerText;
-          
-          // closure for immediate execution
-          (function (_originalHtml) {
-            texts[i].addEventListener("mouseenter", function(e) {
-              e.target.innerHTML = _originalHtml;
-            });
-            
-            texts[i].addEventListener("mouseleave", function(e) {
-              e.target.textContent = spoilerText;
-            });
-          })(originalHtml);
-          
-          isSpoiler = true;
+        // for each word in page text element e.g. inside <p>
+        for (l = 0; l < words.length; l++) {
+
+          if (words[l] == spoiler) {
+            // TODO: something here about retaining element height? so it's not so jarring to switch between spoiler and hover-over mode
+            texts[j].textContent = spoilerText;
+            texts[j].className += ' ' + spoilerSetClass;
+
+            // closure for immediate execution
+            (function (_originalHtml) {
+              texts[j].addEventListener("mouseenter", function(e) {
+                e.target.innerHTML = _originalHtml;
+              });
+
+              texts[j].addEventListener("mouseleave", function(e) {
+                e.target.textContent = spoilerText;
+              });
+            })(originalHtml);
+
+            isSpoiler = true;
+            break;
+          }
+        }
+
+        // already found spoiler candidate - return to continue on
+        if (isSpoiler) {
           break;
         }
-      }
-      
-      // already found spoiler candidate - return to continue on
-      if (isSpoiler) {
-        break;
       }
     }
   }
 };
+
+// lifted from https://stackoverflow.com/questions/18405736/is-there-a-c-sharp-string-format-equivalent-in-javascript
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
